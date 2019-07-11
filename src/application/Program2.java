3c72d607ef75f886ad38cf1d0ef8be766004e5d8
Marcus <cliquet@gmail.com>
2019-07-11 15:12:39 -0300

    Department insert implementation

---
+++ src/application/Program2.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== Test 1: department insert ====");
+		Department newDepartment = new Department(null, "Food");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		
+		sc.close();
+	}
+
+}
--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -5,12 +5,9 @@ import java.util.List;
 import model.entities.Department;
 
 public interface DepartmentDao {
-	
 	void insert(Department obj);
 	void update(Department obj);
 	void deleteByID(Integer id);
 	Department findByID(Integer id);
 	List<Department> findAll();
-	
-
 }
